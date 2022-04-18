30f0104f677b9f03b73dd3ebe1b3eec8406cd22f
mzmeyev <mzmeyev@sunpowercorp.com>
2022-04-18 14:10:29 +0300

    Homework 5

--- src/test/java/Homework5/TestRunner.java
+++ src/test/java/Homework5/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty",
         features = {"C:\\Users\\Dell\\Homework5\\src\\test\\resources\\feature"},
-        glue = {"C:\\Users\\Dell\\Homework5\\src\\test\\java\\Homework5\\steps"},
+        glue = {"\\Homework5\\steps"},
         tags = "@forumcinemas"
 
 )
